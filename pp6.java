24fb45b6c9d7f03ac343d7c0fe113bd7cbfdf375
Nikhil Tiwari <69097170+nikhiltiwari008@users.noreply.github.com>
2022-11-09 14:05:50 +0530

    Add files via upload

---
+++ pp6.java
@@ -0,0 +1,15 @@
+public class pp6 {
+
+    public static void main(String[] args) {
+        int i,j,n=4;
+        for(i=1; i<=n; i++)
+        {
+            for(j=1; j<=i; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+    }
+
