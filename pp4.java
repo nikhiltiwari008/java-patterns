ad876a9b9bafb298fc22dfdffb7a09aa000d622b
Nikhil Tiwari <69097170+nikhiltiwari008@users.noreply.github.com>
2022-11-09 13:58:16 +0530

    Add files via upload

---
+++ pp4.java
@@ -0,0 +1,20 @@
+public class pp4 
+{
+            public static void main(String[] args) {
+                int i,j,n=4;
+                for(i=1; i<=n; i++)
+                {
+                    for(j=i; j<=n; j++)
+                    {
+                        System.out.print(" ");
+                    }
+                    for(int k=1; k<=i; k++)
+                    {
+                    System.out.print("*");
+                    }
+                    System.out.println("");
+
+                }
+            }
+        }
+        
