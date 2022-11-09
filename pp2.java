f05474da91c0c3417ff30bff80253d3ada748966
Nikhil Tiwari <69097170+nikhiltiwari008@users.noreply.github.com>
2022-11-09 13:43:04 +0530

    Add files via upload

---
+++ pp2.java
@@ -0,0 +1,20 @@
+public class pp2 {
+    
+        public static void main(String[] args) {
+            int i,j,n=5;
+            for(i=1; i<=n; i++)
+            {
+                for(j=1; j<=n; j++)
+                {
+                    if(i==1|| j==1 || i==n || j==n)
+                    {
+                    System.out.print("*");
+                    }
+                    else{ System.out.print(" ");}
+                }
+                System.out.println("");
+            }
+        }
+ 
+    
+}
