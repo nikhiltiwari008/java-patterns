41ebaff87de5cc0267857389f99fd63ba9a91293
Nikhil Tiwari <69097170+nikhiltiwari008@users.noreply.github.com>
2022-11-09 14:07:41 +0530

    Add files via upload

---
+++ pp9.java
@@ -0,0 +1,50 @@
+public class pp9 {
+    
+    public static void main(String[] args) {
+        int i,j,n=4;
+        for(i=1; i<=n; i++)
+        {
+            for(j=1; j<=i; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        for(i=1; i<=n; i++)
+        {
+            for(j=i; j<=n; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        //int i,j,n=4;
+        for(i=1; i<=n; i++)
+        {
+            for(j=i; j<=n; j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k=1; k<=i; k++)
+            {
+            System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+
+            //int i,j,n=4;
+            for(i=1; i<=n; i++)
+            {
+                for(j=1; j<=i; j++)
+                {
+                    System.out.print(" ");
+                }
+                for(int k=i; k<=n; k++)
+                {
+                System.out.print("*");
+                }
+                System.out.println("");
+            }
+    }
+}
