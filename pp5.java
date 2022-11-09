5195aa511dd151a9e91995f61b312cd9e583bad2
Nikhil Tiwari <69097170+nikhiltiwari008@users.noreply.github.com>
2022-11-09 13:58:44 +0530

    Add files via upload

---
+++ pp5.java
@@ -0,0 +1,19 @@
+public class pp5 
+{    
+            public static void main(String[] args) {
+                int i,j,n=4;
+                for(i=1; i<=n; i++)
+                {
+                    for(j=1; j<=i; j++)
+                    {
+                        System.out.print(" ");
+                    }
+                    for(int k=i; k<=n; k++)
+                    {
+                    System.out.print("*");
+                    }
+                    System.out.println("");
+
+                }
+            }
+        }
